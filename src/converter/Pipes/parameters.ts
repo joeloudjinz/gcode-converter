ab4f92e1085d1ec6aec864a1f7f894c2da4637df
abdellah addoun <addoun.abdellah@gmail.com>
2019-11-22 17:43:27 +0100

    Determining the use of laser mode

--- src/converter/Pipes/parameters.ts
+++ src/converter/Pipes/parameters.ts
@@ -1,4 +1,10 @@
-import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common';
+import {
+  PipeTransform,
+  Injectable,
+  ArgumentMetadata,
+  BadRequestException,
+  HttpStatus,
+} from '@nestjs/common';
 import { Parameters } from '../Dto/parameters.dto';
 import { FeedRate } from '../Dto/feed-rate.dto';
 import { Invest } from '../Dto/invest.dto';
@@ -6,13 +12,29 @@ import { Laser } from '../Dto/laser.dto';
 
 @Injectable()
 export class ConversionParametersParser implements PipeTransform {
-  parseFeedRate(old: any): FeedRate {
+  private parseFeedRate(old: any): FeedRate {
     if (old.feedRate) {
       const feedRate = JSON.parse(old.feedRate);
       return new FeedRate(parseInt(feedRate.work), parseInt(feedRate.idle));
     }
     return new FeedRate();
   }
+
+  private shouldUseLaser(old: any): Laser {
+    if (!old.laserMode || old.laserMode == false) {
+      return null;
+    }
+    const laser = JSON.parse(old.laser);
+    if (!laser.commandPowerOn || !laser.commandPowerOff) {
+      throw new BadRequestException({
+        status: HttpStatus.BAD_REQUEST,
+        error:
+          'Laser mode configuration is not correct, provide both power strings',
+      });
+    }
+    return new Laser(old.commandPowerOn, old.commandPowerOff);
+  }
+
   transform(old: any, metadata: ArgumentMetadata) {
     return new Parameters(
       parseInt(old.toolDiameter),
@@ -24,7 +46,7 @@ export class ConversionParametersParser implements PipeTransform {
       parseInt(old.blackZ),
       parseInt(old.safeZ),
       this.parseFeedRate(old),
-      new Laser(),
+      this.shouldUseLaser(old),
     );
   }
 }

d2f4ee1ce5b370fb349d7140f376ba73e259d9b8
abdellah addoun <addoun.abdellah@gmail.com>
2019-11-25 11:34:25 +0100

    Decorating parameters dto attributes

--- src/converter/Dto/parameters.dto.ts
+++ src/converter/Dto/parameters.dto.ts
@@ -1,19 +1,32 @@
 import { Laser } from './laser.dto';
 import { FeedRate } from './feed-rate.dto';
 import { Invest } from './invest.dto';
+import { IsDefined, IsOptional } from 'class-validator';
 
 export class Parameters {
+  @IsDefined()
   public toolDiameter: number = 1;
+  @IsDefined()
   public sensitivity: number = 0.95;
+  @IsDefined()
   public scaleAxes: number = 200;
+  @IsDefined()
   public deepStep: number = -1;
+  @IsOptional()
   public invest: Invest = new Invest();
+  @IsDefined()
   public whiteZ: number = 0;
+  @IsDefined()
   public blackZ: number = -2;
+  @IsDefined()
   public safeZ: number = 2;
+  @IsOptional()
   public feedRate: FeedRate = new FeedRate();
+  @IsDefined()
   public laserMode: boolean;
+  @IsOptional()
   public laser: Laser = new Laser();
+  @IsOptional()
   public dirImg: string;
 
   constructor(
